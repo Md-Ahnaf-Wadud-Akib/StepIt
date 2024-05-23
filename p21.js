5766114855cdd00fb4ade75585cfb334f8eaf37a
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-05-23 16:55:50 +0600

    toLowerCase

--- p21.js
+++ p21.js
@@ -1,4 +1,11 @@
-var firstName = "Ahnaf", lastName = "Akib", fullName;
+// toLowerCase
 
-fullName = firstName + " "+ lastName;
-console.log("My full name is", fullName);
+var person1 = "Ahnaf";
+var person2 = "AhnaF";
+
+if(person1.toLowerCase === person2.toLowerCase){
+    console.log("Person1 and Person2 are the same person");
+}
+else{
+    console.log("they are two different person");
+}
