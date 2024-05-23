5cf059604b7c077d1829083341d7f497a9255963
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-05-23 17:01:30 +0600

    toUpperCase

--- p21.js
+++ p21.js
@@ -1,11 +1,23 @@
 // toLowerCase
 
-var person1 = "Ahnaf";
-var person2 = "AhnaF";
+// var person1 = "Ahnaf";
+// var person2 = "AhnaF";
 
-if(person1.toLowerCase === person2.toLowerCase){
-    console.log("Person1 and Person2 are the same person");
+// if(person1.toLowerCase === person2.toLowerCase){
+//     console.log("Person1 and Person2 are the same person");
+// }
+// else{
+//     console.log("they are two different person");
+// }
+
+// toUpperCase
+
+var admin = "Ahanf";
+var pass = "Ahnaf";
+
+if(admin.toUpperCase === pass.toUpperCase){
+    console.log("login");
 }
 else{
-    console.log("they are two different person");
+    console.log("invalid user");
 }
