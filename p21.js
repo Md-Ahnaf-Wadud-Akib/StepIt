4f801889b9dfdeae67e3e26fb89b9e0b46e29287
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-05-27 21:06:37 +0600

    practice problem 2

--- p21.js
+++ p21.js
@@ -118,4 +118,9 @@
 // [num1, num2] = [num2, num1];
 // console.log(num1, num2);
 
-console.log("Hello World");
+var num1 = 12;
+var num2 = 8;
+var sum = 0;
+
+sum = num1 + num2;
+console.log("your sum is: ", sum);
