7bba40956f41fbbfe947bdc5d3ee9e341fdd1c0f
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-06-02 13:22:33 +0600

    concat

--- p22.js
+++ p22.js
@@ -1,3 +1,9 @@
-let marks = [2, 3, 4, 5, 6, 7, 8];
+// let marks = [2, 3, 4, 5, 6, 7, 8];
 
-console.log(Array.isArray(marks));
+// console.log(Array.isArray(marks));
+// console.log(marks.indexOf(8));
+
+let even =[2, 4, 6, 8];
+let odd = [1, 3, 5, 7];
+let allNumbers = even.concat(odd);
+console.log(allNumbers);
