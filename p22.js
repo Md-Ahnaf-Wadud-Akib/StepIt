c069580de400da9e74d3d4d2d1a11fdd096251a9
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-06-02 13:46:12 +0600

    array slice and splice

--- p22.js
+++ p22.js
@@ -6,4 +6,12 @@
 let even =[2, 4, 6, 8];
 let odd = [1, 3, 5, 7];
 let allNumbers = even.concat(odd);
-console.log(allNumbers);
+// console.log(allNumbers);
+
+let partial = even.slice(1, 3);
+console.log(partial);
+console.log(even);
+
+let partial1 = odd.splice(1, 2);
+console.log(partial1);
+console.log(odd);
