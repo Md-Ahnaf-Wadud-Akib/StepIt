af3dab6b94e943759172da13fc63b2553009d6fe
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-06-03 21:16:15 +0600

    practice problem 16

--- p22.js
+++ p22.js
@@ -3,15 +3,42 @@
 // console.log(Array.isArray(marks));
 // console.log(marks.indexOf(8));
 
-let even =[2, 4, 6, 8];
-let odd = [1, 3, 5, 7];
-let allNumbers = even.concat(odd);
-// console.log(allNumbers);
+// let even =[2, 4, 6, 8];
+// let odd = [1, 3, 5, 7];
+// let allNumbers = even.concat(odd);
+// // console.log(allNumbers);
 
-let partial = even.slice(1, 3);
-console.log(partial);
-console.log(even);
+// let partial = even.slice(1, 3);
+// console.log(partial);
+// console.log(even);
 
-let partial1 = odd.splice(1, 2);
-console.log(partial1);
-console.log(odd);
+// let partial1 = odd.splice(1, 2, 99, 88, 77);
+// console.log(partial1);
+// console.log(odd);
+
+function picnic(inputArray){
+  let abcArray = [];
+  for(let i = 0; i < inputArray.length; i++){
+    let element = inputArray[i];
+    if(abcArray.includes(element) === false){
+      abcArray.push(element);
+    }
+  }
+  return abcArray;
+}
+
+let students = [
+  "Ahnaf",
+  "Akib",
+  "Fahmida",
+  "Protiva",
+  "Rahman",
+  "Protiva",
+  "Akib",
+  "Humayra",
+  "Humayra",
+];
+
+let result = picnic(students);
+console.log(result);
+
