74994ea6403254f867dc14dc4fae9f45dc80661e
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-06-06 11:44:14 +0600

    practice problem 21

--- p22.js
+++ p22.js
@@ -171,23 +171,53 @@
 // let result = sumOfProductPrice(shoppingCart);
 // console.log(result);
 
-function sumOfProductPrices(input){
-  let sum = 0;
-  for(i = 0; i < input.length; i++){
-    let index = i;
-    let element = input[index];
-    let individualProductCost = element.productPrice * element.productQuantity;
-    sum = sum + individualProductCost;
-  }
-  return sum;
+// function sumOfProductPrices(input){
+//   let sum = 0;
+//   for(i = 0; i < input.length; i++){
+//     let index = i;
+//     let element = input[index];
+//     let individualProductCost = element.productPrice * element.productQuantity;
+//     sum = sum + individualProductCost;
+//   }
+//   return sum;
+// }
+
+// let shoppingCart = [
+//   { productName: "Shirt", productPrice: 500, productQuantity: 2 },
+//   { productName: "Pant", productPrice: 700, productQuantity: 3 },
+//   { productName: "Shoe", productPrice: 1200, productQuantity: 1 },
+//   { productName: "Cap", productPrice: 50, productQuantity: 5 },
+// ];
+
+// let result = sumOfProductPrices(shoppingCart);
+// console.log(result);
+
+function ticketPrice(ticketQuantity){
+    const first100TicketPrice = 100;
+    const second100TicketPrice = 90;
+    const third100TicketPrice = 80;
+    if(ticketQuantity <= 100){
+        let totalTicketPrice = first100TicketPrice * ticketQuantity;
+        return totalTicketPrice;
+    }
+    else if(ticketQuantity <= 200){
+        let first100TicketRate = first100TicketPrice * 100;
+        let extraTicket = ticketQuantity - 100;
+        let extraTicketRate = extraTicket * second100TicketPrice;
+        let totalTicketPrice200 = first100TicketRate + extraTicketRate;
+        return totalTicketPrice200;
+    }
+    else{
+        let first100rate = first100TicketPrice * 100;
+        // let second100 = ticketQuantity - 100;
+        let second100Rate = 100 * second100TicketPrice;
+        let third100 = ticketQuantity - 200;
+        let third100Rate = third100 * third100TicketPrice;
+        let totalTicketprice300 = first100rate + second100Rate + third100Rate;
+        return totalTicketprice300;
+    }
 }
 
-let shoppingCart = [
-  { productName: "Shirt", productPrice: 500, productQuantity: 2 },
-  { productName: "Pant", productPrice: 700, productQuantity: 3 },
-  { productName: "Shoe", productPrice: 1200, productQuantity: 1 },
-  { productName: "Cap", productPrice: 50, productQuantity: 5 },
-];
-
-let result = sumOfProductPrices(shoppingCart);
-console.log(result);
+let numberOfTicket = 320;
+let result = ticketPrice(numberOfTicket);
+console.log("The price of all tickets is:", result);
