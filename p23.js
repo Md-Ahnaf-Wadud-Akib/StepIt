d85d92fec66b312afa4d27c06955a5e8f1f1b841
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-06-08 21:25:59 +0600

    image

--- p23.js
+++ p23.js
@@ -1,6 +1,7 @@
 function stringMatch(string1, string2){
     if(typeof string1 !== "string" || typeof string2 !== "string"){
         console.log("Please enter valid string!!");
+        return false;
     }
     else{
         
@@ -14,8 +15,8 @@ function stringMatch(string1, string2){
     
 }
 
-let input1 = "John";
-let input2 = "ohn";
+let input1 = "JavaScript";
+let input2 = 23;
 
 let result = stringMatch(input1, input2);
 console.log(result);
