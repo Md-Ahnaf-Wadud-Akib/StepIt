3cc6956d13ddc3cb9530a06e673b0ec89cb44988
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-07-11 15:29:45 +0600

    reverse an array

---
+++ array.js
@@ -0,0 +1,40 @@
+// let friendsAge = [12, 23 ,34 ,56 ,78, 90];
+// // let arrayLength = friendsAge.length;
+
+// let thirdAge = friendsAge[2];
+// console.log(thirdAge);
+
+// // console.log(arrayLength);
+
+// m    a   h   i   b
+
+// mahib = biham
+
+let array = "mahib";
+// console.log(array.length);
+
+for(let i = array.length; i >= 0; i--){
+
+
+
+    let element = array[i];
+    console.log(element);
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
