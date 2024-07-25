363809cb49aa8f695505c120c89900c045ae4327
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-07-25 20:51:55 +0600

    abc

--- p23.js
+++ p23.js
@@ -1,37 +1,58 @@
-function stringMatch(string1, string2){
-    if(typeof string1 !== "string" || typeof string2 !== "string"){
-        console.log("Please enter valid string!!");
-        return false;
-    }
-    else{
+// function stringMatch(string1, string2){
+//     if(typeof string1 !== "string" || typeof string2 !== "string"){
+//         console.log("Please enter valid string!!");
+//         return false;
+//     }
+//     else{
         
-        if(string1.includes(string2)){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
+//         if(string1.includes(string2)){
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
     
-}
+// }
 
-let input1 = "JavaScript";
-let input2 = 23;
+// let input1 = "JavaScript";
+// let input2 = 23;
 
-let result = stringMatch(input1, input2);
-console.log(result);
+// let result = stringMatch(input1, input2);
+// console.log(result);
 
-function getCurrentDateTime() {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0');
-    const day = String(now.getDate()).padStart(2, '0');
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
+// function getCurrentDateTime() {
+//     const now = new Date();
+//     const year = now.getFullYear();
+//     const month = String(now.getMonth() + 1).padStart(2, '0');
+//     const day = String(now.getDate()).padStart(2, '0');
+//     const hours = String(now.getHours()).padStart(2, '0');
+//     const minutes = String(now.getMinutes()).padStart(2, '0');
+//     const seconds = String(now.getSeconds()).padStart(2, '0');
     
-    const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-    console.log(formattedDateTime);
-}
+//     const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+//     console.log(formattedDateTime);
+// }
 
-getCurrentDateTime();
+// getCurrentDateTime();
+
+
+
+// var inputName = "Paw's Step";
+
+// switch (inputName) {
+//   case "Oreo":
+//     console.log("the name is Oreo");
+//     break;
+//   case "Timtim":
+//     console.log("the name is Timtim");
+//     break;
+//   case "Minmin":
+//     console.log("the name is Minmin");
+//     break;
+//   case "Paw's Step":
+//     console.log("the name is Paw's Step");
+//     break;
+//   default:
+//     console.log("there is no name found matched");
+// }
