fb4593c7f46ad1c0111f7450aa60e95ad64aa484
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-09-17 17:34:32 +0600

    keyboard keys id added

--- typing project/js/alpha.js
+++ typing project/js/alpha.js
@@ -1,7 +1,17 @@
 let introBtn = document.getElementById("intro-btn");
 
+function continueGame(){
+    let alphabet = getAlphabet();
+
+    let displayAlphabet = document.getElementById("display-alphabet");
+    displayAlphabet.innerText = alphabet;
+
+    setBackgroundColor(alphabet);
+}
+
 introBtn.addEventListener("click", function(){
     hideItemUsingId("intro-section");
-    showItemUsingId("playground-section")
+    showItemUsingId("playground-section");
+    continueGame()
 })
 
--- typing project/js/utility.js
+++ typing project/js/utility.js
@@ -7,3 +7,21 @@ function showItemUsingId(inputId){
     let section = document.getElementById(inputId);
     section.classList.remove("hidden");
 }
+
+function getAlphabet(){
+    let alphabetString = "abcdefghijklmnopqrstuvwxyz";
+    let alphabets = alphabetString.split("");
+    
+    let randomNumber = Math.round(Math.random()*25);
+    let index = randomNumber;
+
+    let alphabet = alphabets[index];
+    return alphabet;
+
+
+}
+
+function setBackgroundColor(inputId){
+    let alphabetKey = document.getElementById(inputId);
+    alphabetKey.classList.add("bg-orange-400", "text-black");
+}
