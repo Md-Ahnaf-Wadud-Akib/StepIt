dfd7fb33959fcea48528aa16f15872967cf88127
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2025-02-18 10:57:46 +0600

    deposit screen added

--- goPay/js/gopay.js
+++ goPay/js/gopay.js
@@ -7,3 +7,13 @@ withdrawalBtn.addEventListener("click", function () {
   const withdrawalScreen = document.getElementById("withdrawal-screen");
   withdrawalScreen.classList.toggle("hidden");
 });
+
+const depositBtn = document.getElementById("deposit-btn");
+
+depositBtn.addEventListener("click", function () {
+  const homeScreen = document.getElementById("home-screen");
+  homeScreen.classList.add("hidden");
+
+  const depositScreen = document.getElementById("deposit-screen");
+  depositScreen.classList.toggle("hidden");
+});
