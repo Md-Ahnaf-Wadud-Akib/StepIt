64de1d4ffa7f4231c508181926b2ae558c2075d7
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2025-02-21 15:16:59 +0600

    upgrade

--- goPay/js/gopay.js
+++ goPay/js/gopay.js
@@ -17,3 +17,21 @@ depositBtn.addEventListener("click", function () {
   const depositScreen = document.getElementById("deposit-screen");
   depositScreen.classList.toggle("hidden");
 });
+
+// Withdrawal Screen
+const withdrawalSubmit = document.getElementById("withdrawal-submit");
+withdrawalSubmit.addEventListener("click", function () {
+  const amountText = document.getElementById("amount-text").value;
+  const amountNumber = parseFloat(amountText);
+  const pinPassword = document.getElementById("pin-password").value;
+
+  if (amountNumber && pinPassword === " ") {
+    alert("Please enter the amount and your password");
+  } else {
+    if (pinPassword === "987") {
+      
+    } else {
+      alert("Your inserted password is wrong");
+    }
+  }
+});
